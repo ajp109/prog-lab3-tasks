6d1f76829d5da208acb235db5c365d2a67295e9a
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-04 16:56:55 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include "mbed.h"
 
-int main() {
-    while (true) {
-        sleep();
-    }
+void test() {
+    int count = 3;
+    printf("In test(), count = %d\r\n", count);
+}
+
+int main()
+{
+    int count = 5;
+    
+    test();
+    
+    printf("In main(), count = %d\r\n", count);
+    
+    // Do nothing, forever, when the program is complete
+    while (true) sleep();
 }
